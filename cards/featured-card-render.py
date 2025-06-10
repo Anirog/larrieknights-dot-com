36749144ae75036bc3bfe094cf98f4dcf5b92f0a
Larrie <larrieknights@gmail.com>
2025-06-10 13:33:24 +0100

    Add featured card of my own design

--- cards/featured-card-render.py
+++ cards/featured-card-render.py
@@ -2,12 +2,12 @@ import json
 from jinja2 import Environment, FileSystemLoader
 
 # Step 1: Load the card data from JSON
-with open('cards.json', 'r') as f:
+with open('featuredcards.json', 'r') as f:
     cards = json.load(f)
 
 # Step 2: Set up Jinja2 to load from current folder
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('template.html')
+template = env.get_template('featured-card-template.html')
 
 # Step 3: Pick the first card
 card = cards[0]
@@ -16,5 +16,5 @@ card = cards[0]
 output = template.render(card=card)
 
 # Step 5: Save the result to an HTML file
-with open('rendered-card.html', 'w') as f:
+with open('featured-card.html', 'w') as f:
     f.write(output)
